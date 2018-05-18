8b189c268fb48d13301d35f2a8d48a5c29fb2153
Paul DE RENTY <pde-rent@e1r1p14.42.fr>
2018-05-18 10:29:05 +0200

    header

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -97,6 +97,7 @@ int				slen(char *s);
 long			labs(long n);
 int				is_space(const char c);
 int				is_digit(const char c);
+int				active_bits(char c);
 int				plong(int fd, long n, char end);
 int				pstr(int fd, const char *s, char end);
 int				scat(char *dest, const char *src, char c);
