002f5df332bf19d0b0ca0f11f5aa533ab2023a87
Paul de Renty <pde-rent@student.42.fr>
2018-05-23 11:50:32 +0200

    push_swap >> objs cleanup

--- lem_in/includes/lem_in.h
+++ lem_in/includes/lem_in.h
@@ -1,4 +1,22 @@
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
 #include "anthill.h"
+
+/*
+ * BASICS
+ */
+
+int				slen(char *s);
+long			labs(long n);
+int				is_space(const char c);
+int				is_digit(const char c);
+int				active_bits(char c);
+int				plong(int fd, long n, char end);
+int				pstr(int fd, const char *s, char end);
+int				scat(char *dest, const char *src, char c);
+void			init_env(t_env *env, int ac);
+void			deinit_env(t_env *env);
