dbf55dc470d63b0794c22f9ea39eb724b5c37ea0
Paul DE RENTY <pde-rent@e3r8p21.42.fr>
2018-05-31 07:19:30 +0200

    bruteforce added for < 12

--- push_swap/srcs/tiny_bruteforce.c
+++ push_swap/srcs/tiny_bruteforce.c
@@ -12,50 +12,73 @@
 
 #include "push_swap.h"
 
-static int		mv_lst(t_env *env, char which, int mv)
+static void		mv_lst(t_env *env, char which,
+	long *pile, int max_depth, int nb_mv)
 {
-	if (mv == 1)
-		swap(env, which);
-	else if (mv == 2 && !(env->this_move->id[1] == 'r'))
-		rotate(env, which);
-	else if (mv == 3
-		&& !(env->this_move->id[0] == 'r' && env->this_move->id[1] == which))
-		reverse_rotate(env, which);
-	else
-		return (0);
-	return (1);
+	static int	first = 0;
+	static int	mv = 3;
+	static int	i = 0;
+	t_move		*cache;
+
+	cache = NULL;
+	//if (!is_sort(pile, 0, env->size - 1))
+	if (max_depth > 0)
+	{
+		dprintf(2, "which: %c max_depth: %d nb_mv: %d\n", which, max_depth, nb_mv);
+		if (nb_mv == 0)
+		{
+			//dprintf(2, "which: %c max_depth: %d\n", which, max_depth);
+			i = 0 - first;
+			mv = ((mv + 1) > 3 ? (mv - 2) : (mv + 1));
+			(!(mv % 3) ? --max_depth : 0);
+			while (++i <= max_depth){
+				dprintf(2, "i: %d max_depth: %d\n", i, max_depth);
+				undo_move(env, env->this_move);
+				//put_moves(env->first_move, 1, ' ');
+			}
+			dprintf(2, "mv: %d\n", mv);
+			//if (mv == 2 && !(env->this_move->id[1] == 'r'))
+			if (mv == 2)
+				rotate(env, which);
+			//else if (mv == 3 && !(env->this_move->id[0] == 'r'
+			//	&& env->this_move->id[1] == which))
+			else if (mv == 3)
+				reverse_rotate(env, which);
+			else
+				swap(env, which);
+			nb_mv = max_depth - 1;
+			//first = 1;
+
+		}
+		else if (max_depth > 0)
+		{
+			swap(env, which);
+			is_sort(pile, 0, env->size - 1) ? cache_moves(env, cache) : 0;
+			undo_move(env, env->this_move);
+//			if (!(env->this_move && env->this_move->id[1] == 'r'))
+//			{
+			rotate(env, which);
+			is_sort(pile, 0, env->size - 1) ? cache_moves(env, cache) : 0;
+			undo_move(env, env->this_move);
+//			}
+//			if (!(env->this_move && env->this_move->id[0] == 'r' && env->this_move->id[1] == which))
+			reverse_rotate(env, which);
+			is_sort(pile, 0, env->size - 1) ? cache_moves(env, cache) : 0;
+			--nb_mv;
+			//undo_move(env, env->this_move);
+		}
+		mv_lst(env, which, pile, max_depth, nb_mv);
+	}
+	get_cache(env, cache);
 }
 
 void			bruteforce(t_env *env, char which)
 {
-	int		i;
-	int		j;
-	int		k;
-	int		nb_mv;
 	int		max_mv;
-	int		start;
-	long	**pile;
-	long	tmp_pile[env->size];
+	long	*pile;
 
-	*pile = (which == 'a' ? env->a : env->b);
-	start = (which = 'a' ? env->a1 : env->b1);
-	max_mv = ((which = 'a' ? LEN_A : LEN_B) * 3);
-	duplicate_pile(*pile, tmp_pile, start, env->size - 1);
-	nb_mv = 0;
-	while (++nb_mv <= max_mv)
-	{
-		i = 0;
-		while (++i <= nb_mv)
-		{
-			j = 0;
-			while (++j <= 3){
-				mv_lst(env, which, nb_mv);
-			}
-			if (is_sort(*pile, start, env->size - 1))
-				return ;
-			del_moves(env);
-			duplicate_pile(tmp_pile, *pile, start, env->size - 1);
-		}
-	}
-	return (best_step);
+	pile = env->a;
+	(which == 'a') ? pile = env->a : 0;
+	max_mv = ((which == 'a' ? LEN_A : LEN_B) * 3);
+	mv_lst(env, which, pile, max_mv, max_mv);
 }
