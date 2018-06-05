21dc87d8444ca1720e6ee9ff13a23149ded6ffd4
jyildiz <jyildiz-@student.42.fr>
2018-06-05 17:40:35 +0200

    print_move

--- lem_in/srcs/anthill_builder.c
+++ lem_in/srcs/anthill_builder.c
@@ -6,7 +6,7 @@
 /*   By: jyildiz- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 16:49:05 by jyildiz-          #+#    #+#             */
-/*   Updated: 2018/06/05 17:19:39 by jyildiz-         ###   ########.fr       */
+/*   Updated: 2018/06/05 17:40:16 by jyildiz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void		move_ant(s_env *env, int i)
 		env->colony[i]->path->current->ant = NULL;
 		env->colony[i]->path->current = env->colony[i]->path->next;
 		env->colony[i]->path->current->ant = env->colony[i];
+		pstr(1, "L", '\0');
+		plong(1, i, '\0');
+		pstr(1, "-", '\0');
+		pstr(1, &(env->colony[i]->path->current->id[0]), '\0');
 	}
 }
 
@@ -32,4 +36,30 @@ void		move_colony(s_env *env)
 		i++;
 	}
 }
+
+void		print_move(s_env *env)
+{
+	int		i;
+
+	i = 0;
+	while (env->colony[env->nb_ants - 1]->path->current != env->end)
+	{
+		move_colony(env);
+		pstr(1, "\n", '\0');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
