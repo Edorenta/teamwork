cb111d2be59bb57ab15e10bc4a51a0168ea33f99
Florian MADURA <fmadura@student.42.fr>
2018-06-13 09:59:16 +0200

    clean

--- Corewar/includes/asm.h
+++ Corewar/includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 12:36:47 by fmadura           #+#    #+#             */
-/*   Updated: 2018/06/11 18:44:00 by fmadura          ###   ########.fr       */
+/*   Updated: 2018/06/12 21:04:52 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 # include "ft_printf.h"
 # include "libft.h"
 # include "op.h"
-
+typedef struct s_op
+{
+	char	*name;
+	int		nlen;
+	int		nbarg;
+	int		args[3];
+	int		opcod;
+	int		cycle;
+	char	*desc;
+	int		octal;
+	int		label;
+}			t_op;
 #endif
