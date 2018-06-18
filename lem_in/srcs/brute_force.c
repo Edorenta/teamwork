c53343c3340168df6fb3a55c86357161d1aff256
jyildiz <jyildiz-@student.42.fr>
2018-06-18 15:28:09 +0200

    corrige bug sur brute force

--- lem_in/srcs/brute_force.c
+++ lem_in/srcs/brute_force.c
@@ -18,6 +18,22 @@ static int		alrdy_use(t_path *path, t_room *next)
 	return (0);
 }
 
+static int		used(t_path *path, t_link *tmp)
+{
+	t_path *pathtmp;
+
+	pathtmp = path;
+	while (pathtmp->prev)
+	{
+		if (pathtmp->room == tmp->room)
+			return (1);
+		pathtmp = pathtmp->prev;
+	}
+	if (pathtmp->room == tmp->room)
+		return (1);
+	return (0);
+}
+
 static void		del_ant_next(t_path *path)
 {
 	t_link	*tmp;
@@ -25,7 +41,8 @@ static void		del_ant_next(t_path *path)
 	tmp = path->room->link;
 	while (tmp)
 	{
-		tmp->room->ant = NULL;
+		if (!used(path, tmp))
+			tmp->room->ant = NULL;
 		tmp = tmp->next;
 	}	
 }
@@ -34,8 +51,7 @@ static void		del_one_path(t_path *path)
 {
 	t_path *tmp;
 
-	tmp = path;
-	path = path->prev;
+	tmp = path->next;
 	free(tmp);
 	path->next = NULL;
 }
@@ -53,9 +69,9 @@ void		brute_solve(t_env *env)
 	while (path->room != env->end)
 	{
 		if (!alrdy_use(path, tmp->room))
-		{
+		{	
 			path = add_path(env, path, tmp->room);
-			printf("room id: %s\n", path->room->id);
+			path->room->ant = bullshit;
 			tmp = path->room->link;
 		}
 		else
@@ -65,6 +81,7 @@ void		brute_solve(t_env *env)
 			else
 			{
 				del_ant_next(path);
+				path = path->prev;
 				del_one_path(path);
 				tmp = path->room->link;
 			}
--- lem_in/srcs/error.c
+++ lem_in/srcs/error.c
@@ -19,24 +19,15 @@ int			anthill_complete(t_env *env)
 	t_parsed_room *parsed;
 
 	if (env->nb_ants <= 0 || !env->start || !env->end)
-	{
-		//printf("%d, %s, %s\n", env->nb_ants, env->start->id, env->end->id);
 		return (0);
-	}
 	parsed = R1;
 	if (!(parsed && parsed->room && parsed->room->link))
-	{
-		//printf("test2\n");
 		return (0);
-	}
 	while (parsed->next && parsed->next->room)
 	{
 		parsed = parsed->next;
 		if (!(parsed->room->link))
-		{
-			//printf("test2\n");
 			return (0);
-		}
 	}
 	return (1);
 }
