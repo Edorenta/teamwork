54cc17828f549cdfa94074b0fd7263e2aadd2cb0
Paul DE RENTY <pde-rent@e1r6p8.42.fr>
2018-06-21 17:39:32 +0200

    lem-in script

--- lem_in/test.sh
+++ lem_in/test.sh
@@ -5,24 +5,25 @@ GREEN=$(tput setaf 2)
 BASE=$(tput sgr0)
 GOOD_MAPS=checker/good_maps/*
 BAD_MAPS=checker/bad_maps/*
+
 printf ">> ERROR MANAGEMENT\n"
-printf ">> 1. bad maps\n"
+printf ">> 1. BAD MAPS\n"
 for f in $BAD_MAPS
 	do
 		if [[ $(./lem-in < $f 2>&1 | grep "Error") ]]
 		then
-		    printf ">> ${f##*/} ${GREEN}OK${BASE} <<\n"
+		    printf ">> ${GREEN}OK${BASE} << ${f##*/}\n"
 		else
-		    printf ">> ${f##*/} ${RED}KO${BASE} <<\n"
+		    printf ">> ${RED}KO${BASE} << ${f##*/}\n"
 		fi
 	done
-printf ">> 2. good maps\n"
+printf "\n>> 2. GOOD MAPS\n"
 for f in $GOOD_MAPS
 	do
 		if [[ $(./lem-in < $f 2>&1 | grep "Error") ]]
 		then
-		    printf ">> ${f##*/} ${RED}KO${BASE} <<\n"
+		    printf ">> ${RED}KO${BASE} << ${f##*/}\n"
 		else
-		    printf ">> ${f##*/} ${GREEN}OK${BASE} <<\n"
+		    printf ">> ${GREEN}OK${BASE} << ${f##*/}\n"
 		fi
 	done
