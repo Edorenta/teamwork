76c06bf6d5d5db8312cdd361e76410a6a55adc73
Jimmy JOURNE <jjourne@e2r9p17.42.fr>
2018-06-27 19:38:52 +0200

    update

--- Corewar/srcs/vm/show.c
+++ Corewar/srcs/vm/show.c
@@ -42,20 +42,20 @@ void		show_mem(t_vm *vm)
 void		send_mem(t_vm *vm)
 {
 	int		i;
-	char *c;
+	char **c;
 
 	i = 0;
+	c = malloc(sizeof(char *));
 	// ft_printf("0x0000 : ");
 	while (i < MEM_SIZE) //on parcours toutes la memoire
 	{
-		// if (!(i % 64) && i) //seulement si i existe, et ..........
-			// ft_printf("%#.4x : ", i); //affichage de l'adresse (precision de 4, avec l'hexa)
-			// ft_printf("%#.4x : ", i); //affichage de l'adresse (precision de 4, avec l'hexa)
-		ft_sprintf(c, "%02x ", (unsigned char)vm->ram[i].mem); //affiche le contenu de cette case mem = contenu
-		send_to_socket(vm, c, 0);
+		ft_sprintf(*c, "%02x ", (unsigned char)vm->ram[i].mem); //affiche le contenu de cette case mem = contenu
+		// sprintf(c, "%02x ", (unsigned char)vm->ram[i].mem); //affiche le contenu de cette case mem = contenu
+		send_to_socket(vm, *c, 0);
 		if (i / 64 != (i + 1) / 64)
 			send_to_socket(vm, "\n", 0);
 		i++;
+		free(c);
 	}
 	// ft_printf("\n\n");
 }
