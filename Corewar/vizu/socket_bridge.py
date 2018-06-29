471495930beeb4f81f04c64ee6dd5b3aae3882ad
Paul DE RENTY <pde-rent@e3r5p22.42.fr>
2018-06-29 05:21:29 +0200

    server.py

--- Corewar/vizu/socket_bridge.py
+++ Corewar/vizu/socket_bridge.py
@@ -26,8 +26,7 @@ ws_port = os.getenv("WS_PORT", "8082")
 address = "ws://" + s_uri + ":" + ws_port
 
 # Create a TCP/IP socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# wsock = websocket.create_connection(address)
+
 # Bind the socket to the port
 print("Expecting data on %s:%s [TCP]" % (s_uri, s_port))
 print("Sending data to %s:%s [WebSocket]" % (s_uri, ws_port))
@@ -35,43 +34,48 @@ print("Sending data to %s:%s [WebSocket]" % (s_uri, ws_port))
 # sock.bind((s_uri,int(s_port)))
 # Listen for incoming connections
 # sock.listen(1)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+wsock = websocket.create_connection(address)
+sock.bind((s_uri,int(s_port)))
+sock.listen(1)
 
 def play_corewar():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	wsock = websocket.create_connection(address)
-	sock.bind((s_uri,int(s_port)))
-	sock.listen(1)
 
+	_dbg = 0
 	payload = None
-	new_socket, client_address = sock.accept()
+	(new_socket, client_address) = sock.accept()
 	while 1:
 		buf = None
 		while 1:
 			payload = new_socket.recv(2).decode()
-			if not payload:
-				continue
+			if (not payload or ("<end>" in payload)): #or len(payload) < 2):
+				print("End of payload.\nWaiting for another game to begin!")
+				new_socket.close()
+				return
+				# continue
 			else:
 				if buf:
 					buf += payload
 				else:
 					buf = payload
 			if "\r" in payload:
-				print("%s" % buf, end='')
+				# print("%s" % buf, end='')
+				print("Sending #%d" % _dbg)
+				_dbg += 1
 				wsock.send(buf)
 				buf = None
-				if "<end>" in payload:
-					print("End of payload.\nWaiting for another game to begin!")
-					sock.close()
-					wsock.close()
-					play_corewar()
-
-					# c = sys.stdin.read(1)
-					# if (c == 'y' or c == 'Y'):
-					# 	continue
-					# else:
-					# 	sock.close()
-					# 	wsock.close()
-					# 	sys.exit(0)
-					# 	break
-
-play_corewar()
+			# if ("<end>" in payload): #or len(payload) < 2):
+				# print("End of payload.\nWaiting for another game to begin!")
+				# sock.close()
+				# wsock.close()
+				# return
+				# c = sys.stdin.read(1)
+				# if (c == 'y' or c == 'Y'):
+				# 	continue
+				# else:
+				# 	sock.close()
+				# 	wsock.close()
+				# 	sys.exit(0)
+				# 	break
+while 1:
+	play_corewar()
