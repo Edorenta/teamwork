c7270eb35f8439261a222a26aa95cadc850f1b47
Jimmy JOURNE <jjourne@e3r3p19.42.fr>
2018-07-03 00:56:05 +0200

    update

--- Corewar/srcs/vm/opcode/live.c
+++ Corewar/srcs/vm/opcode/live.c
@@ -6,7 +6,7 @@
 /*   By: jjourne <jjourne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/28 15:04:10 by jjourne           #+#    #+#             */
-/*   Updated: 2018/07/02 18:20:58 by jjourne          ###   ########.fr       */
+/*   Updated: 2018/07/02 23:13:23 by jjourne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	live(t_vm *vm, t_proc *proc)
 		ft_printf("Player %d (%s) is said to be alive\n",
 			num, vm->player[num].name);
 		send_to_socket(vm, "$<liv>[", 7);
-		ft_sprintf(buf, "\"%d\"", num);
+		ft_sprintf(buf, "\"%d\", \"%d\"", num, proc->id);
 		send_to_socket(vm, buf, ft_strlen(buf));
 		send_to_socket(vm, "]", 1);
 	}
