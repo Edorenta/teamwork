ce14ea0b94918e4d015963eb52674492d8d7eadc
Jimmy JOURNE <jjourne@e3r3p19.42.fr>
2018-07-02 21:25:58 +0200

    vizu incognito and kill

--- Corewar/vizu/run.sh
+++ Corewar/vizu/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk --app=http://localhost:8080/html/ --window-size=1000,1000
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --incognito --kiosk --app=http://localhost:8080/html/
