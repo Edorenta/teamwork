29a026582cd7ab27d9d078a387faeee770cf51ab
Florian MADURA <fmadura@student.42.fr>
2018-07-03 03:02:30 +0200

    clearing

--- Corewar/rsrc/examples/compile_champs.sh
+++ Corewar/rsrc/examples/compile_champs.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 CHAMPS=code/*.s
+COR=code/*.cor
 
 for f in $CHAMPS
 	do
-		./asm $f
+		valgrind --log-file="tmp" ./asm $f
+		echo Compile $f :
+		cat tmp | grep "definitely lost"
+		cat tmp | grep "indirectly lost"
+		rm tmp
+		echo
+	done
+mv $COR ./bin1/
+
+for f in $CHAMPS
+	do
+		./zaz_asm $f
+	done
+mv $COR ./bin2/
+
+for f in ./bin1/*.cor
+	do
+		diff <(xxd ./bin1/$f) <(xxd ./bin2/$f)
 	done
--- Corewar/srcs/asm/asm.c
+++ Corewar/srcs/asm/asm.c
@@ -6,23 +6,49 @@
 /*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 12:37:38 by fmadura           #+#    #+#             */
-/*   Updated: 2018/07/02 21:30:13 by fmadura          ###   ########.fr       */
+/*   Updated: 2018/07/03 02:59:34 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include <strings.h>
 
-int		ft_asm(char **argv)
+static int	ft_filename(char *filename, char file[2052])
+{
+	int		len;
+
+	len = ft_strlen(filename);
+	if (len > 2 && len < 2047)
+	{
+		len -= 2;
+		if (filename[len] != '.')
+		{
+			printf("Invalid file, please use an '.s' extension \n");
+			return (0);
+		}
+		if (filename[++len] != 's')
+		{
+			printf("Invalid file, please use an '.s' extension \n");
+			return (0);
+		}
+		ft_strncpy(file, filename, --len);
+		ft_strncpy(&file[len], ".cor", 4);
+		return (1);
+	}
+	else
+		printf("Invalid file size\n");
+	return (0);
+}
+
+int			ft_asm(char **argv)
 {
 	int		fd;
+	char	filename[2052];
 	t_iter	*iter;
 	t_ops	*ops;
 
 	iter = iter_new();
-	if (iter == NULL)
-		return (1);
-	else
+	if (iter != NULL && ft_filename(argv[1], filename))
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (fd > 0)
@@ -31,11 +57,14 @@ int		ft_asm(char **argv)
 			ops = parser(iter, fd, 0);
 			iter->header.prog_size = ops_get_len(ops);
 			ops_get_lab(iter, ops);
-			write_all("test.cor", ops, iter->header);
+			write_all(filename, ops, iter->header);
 			iter_del(iter);
 			ops_del(ops);
 		}
+		else
+			printf("File descriptor invalid, file is missing\n");
 		close(fd);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
