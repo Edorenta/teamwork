98b16e16dabe00308a7833d280785d5ce7b07b17
Paul DE RENTY <pde-rent@e3r3p21.42.fr>
2018-07-03 05:21:40 +0200

    error.c

--- Corewar/srcs/asm/error.c
+++ Corewar/srcs/asm/error.c
@@ -12,47 +12,77 @@
 
 #include "asm.h"
 
-void	put_error_first(t_iter *iter, char *line)
+inline static void	jump(int x, char c)
+{
+	char	s[x];
+	int		i;
+
+	i = -1;
+	while (++i < x - 1)
+		s[i] = ' ';
+	s[i] = c;
+	ft_putnstr(s, x);
+}
+
+inline static char	*strpl(char *src, char from, char to)
+{
+	int		i;
+
+	i = -1;
+	while (++i < ft_strlen(src))
+		src[i] == from ? src[i] = to : 0;
+	return (src);
+}
+
+inline static void	jump_print(const char *msg, char *line, int x, char c)
+{
+	line = strpl(line, '\t', ' ');
+	ft_printf("%s\n", line);
+	jump(x + 1, c);
+	ft_putstr(msg);
+}
+
+void				put_error_first(t_iter *iter, char *line)
 {
 	if (iter->token == LABEL_ERR2)
-		printf("%s\n%*c\nNeed space here\n", line, iter->count + 1, '^');
+		jump_print("\nNeed space here\n", line, iter->count + 1, '^');
 	else if (iter->token == HEAD_ERR1)
-		printf("%s\n%*c\nSpace after name.\n", line, iter->count, '^');
+		jump_print("\nSpace after name.\n", line, iter->count, '^');
 	else if (iter->token == HEAD_ERR2)
-		printf("%s\n%*c\nSpace after comment.\n", line, iter->count, '^');
+		jump_print("\nSpace after comment.\n", line, iter->count, '^');
 	else if (iter->token == HEAD_ERR0)
-		printf("%s\n%*c\nIncorrect word\n", line, iter->count + 1, '^');
+		jump_print("\nIncorrect word\n", line, iter->count + 1, '^');
 	else if (iter->token == NAME_ERR0)
-		printf("%s\n%*c\nName over 128 char.\n", line, iter->count, '^');
+		jump_print("\nName over 128 char.\n", line, iter->count, '^');
 	else if (iter->token == COMT_ERR0)
-		printf("%s\n%*c\nComment over 2048 char\n", line, iter->count, '^');
+		jump_print("\nComment over 2048 char\n", line, iter->count, '^');
 	else if (iter->token == ENDLI_ERR)
-		printf("%s\n%*c\nEndline must be empty.\n", line, ++iter->count, '^');
+		jump_print("\nEndline must be empty.\n", line, ++iter->count, '^');
 	iter_del(iter);
 	exit(0);
 }
 
-void	put_error(t_iter *iter, char *line)
+void				put_error(t_iter *iter, char *line)
 {
 	if (iter->token == INS_ERR0)
-		printf("%s\n%*c\nUndefined Instruction.\n", line, iter->count, '^');
+		jump_print("\nUndefined Instruction.\n", line, iter->count, '^');
 	else if (iter->token == PAR_ERR0)
-		printf("%s\n%*c\nIncorrect parameter.\n", line, iter->count, '^');
+		jump_print("\nIncorrect parameter.\n", line, iter->count, '^');
 	else if (iter->token == PAR_ERR1)
-		printf("%s\n%*c\nInvalid number of args.\n", line, iter->count, '^');
+		jump_print("\nInvalid number of args.\n", line, iter->count, '^');
 	else if (iter->token == PAR_ERR2)
-		printf("%s\n%*c\nInvalid type of parameter.\n", line, iter->count, '^');
+		jump_print("\nInvalid type of parameter.\n", line, iter->count, '^');
 	else if (iter->token == NAME_ERR1)
-		printf("%s\n%*c\nCan't be empty\n", line, iter->count, '^');
+		jump_print("\nCan't be empty\n", line, iter->count, '^');
 	else if (iter->token == LABEL_ERR3)
-		printf("%s\n%*c\nRedefinition of label.\n", line, ++iter->count, '^');
+		jump_print("\nRedefinition of label.\n", line, ++iter->count, '^');
 	else if (iter->token == LABEL_ERR4)
-		printf("%s\n%*c\nSpace after label.\n", line, ++iter->count, '^');
+		jump_print("\nSpace after label.\n", line, ++iter->count, '^');
 	else if (iter->token == 0)
-		printf("%s\nNo name & comment.\n", line);
+		ft_printf("%s\nNo name & comment.\n", line);
 	if (iter->token == READ_ERR)
 	{
-		printf("Read error\n");
+		ft_putstr("Read error\n");
 		exit(0);
 	}
 	put_error_first(iter, line);
